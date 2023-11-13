3a44962878d16af99ea7c9f9762ad275597e55b4
Семенов Лев <130220311+chikiryauxgod@users.noreply.github.com>
2023-11-14 00:01:36 +0500

    _2___2___5_Class.py

--- 2 + 2 = 5 Class/2 + 2 = 5 Class/_2___2___5_Class.py
+++ 2 + 2 = 5 Class/2 + 2 = 5 Class/_2___2___5_Class.py
@@ -1,19 +1,17 @@
 class MyInt:
-    def add(self, num1, num2):
-        if num1 == 2 and num2 == 2:
+    def __init__(self, num):
+        self.num = num
+
+    def add(self, other):
+        if self.num == 2 and other == 2:
             result = 5
         else:
-            result = num1 + num2
+            result = self.num + other
         return result
 
-inst = MyInt()
+inst1 = MyInt(2)
+inst2 = MyInt(2)
 
-result1 = inst.add(2, 3)
-result2 = inst.add(1, 4)
-result3 = inst.add(3, 3)
-result4 = inst.add(2, 2)
+result1 = inst1.add(inst2.num)  
+print("2 + 2 =", result1)
 
-print("2 + 3 =", result1)
-print("1 + 4 =", result2)
-print("3 + 3 =", result3)
-print("2 + 2 =", result4)
