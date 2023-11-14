fe4668f3404dce792df02da13d45b2cf0d47040b
Семенов Лев <130220311+chikiryauxgod@users.noreply.github.com>
2023-11-15 00:57:18 +0500

    Update main.c

--- CLab6/CLab6/main.c
+++ CLab6/CLab6/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_STRING_LENGTH 100
 
@@ -14,7 +15,7 @@ struct Person* GenArray(int numPeople) {
     struct Person* peopleArray = (struct Person*)malloc(numPeople * sizeof(struct Person));
 
     if (peopleArray == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
+        fprintf(stderr, "Ошибка выделения памяти\n");
         exit(1);
     }
 
@@ -22,20 +23,20 @@ struct Person* GenArray(int numPeople) {
 }
 
 void FillArray(struct Person* person) {
-    person->name = (char*)malloc(50 * sizeof(char));
+    person->name = (char*)malloc(MAX_STRING_LENGTH * sizeof(char));
     if (person->name == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
+        fprintf(stderr, "Ошибка выделения памяти\n");
         exit(1);
     }
 
-    printf("Enter name: ");
-    scanf_s("%49s", person->name, 50);  // Updated to prevent buffer overflow
-    printf("Enter age: ");
-    scanf_s("%d", &person->age);
+    printf("Введите имя: ");
+    scanf("%s", person->name);
+    printf("Введите возраст: ");
+    scanf("%d", &person->age);
 }
 
 void PrintArray(const struct Person* person) {
-    printf("Name: %s, Age: %d\n", person->name, person->age);
+    printf("Имя: %s, Возраст: %d\n", person->name, person->age);
 }
 
 struct Person* A_Names(const struct Person* firstArr, int firstSize, int* secondSize) {
@@ -47,13 +48,27 @@ struct Person* A_Names(const struct Person* firstArr, int firstSize, int* second
         }
     }
 
+    if (count == 0) {
+        printf("Требуемые элементы не найдены.\n");
+        *secondSize = 0;
+        return NULL;
+    }
+
     *secondSize = count;
     struct Person* secondArr = GenArray(count);
 
     int secondIndex = 0;
     for (int i = 0; i < firstSize; ++i) {
         if (firstArr[i].name[0] == 'A' || firstArr[i].name[0] == 'a') {
-            secondArr[secondIndex++] = firstArr[i];
+            secondArr[secondIndex].name = (char*)malloc(strlen(firstArr[i].name) + 1);
+            if (secondArr[secondIndex].name == NULL) {
+                fprintf(stderr, "Ошибка выделения памяти\n");
+                exit(1);
+            }
+
+            strcpy(secondArr[secondIndex].name, firstArr[i].name);
+            secondArr[secondIndex].age = firstArr[i].age;
+            secondIndex++;
         }
     }
 
@@ -64,48 +79,59 @@ void FillStrings(char*** stringArray, int size) {
     *stringArray = (char**)malloc(size * sizeof(char*));
 
     if (*stringArray == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
+        fprintf(stderr, "Ошибка выделения памяти\n");
         exit(1);
     }
+
+    // Очистить буфер ввода
+    while (getchar() != '\n');
 
     for (int i = 0; i < size; ++i) {
         (*stringArray)[i] = (char*)malloc(MAX_STRING_LENGTH * sizeof(char));
 
         if ((*stringArray)[i] == NULL) {
-            fprintf(stderr, "Memory allocation error\n");
+            fprintf(stderr, "Ошибка выделения памяти\n");
             exit(1);
         }
 
-        printf("Enter string %d: ", i + 1);
-        scanf_s("%s", (*stringArray)[i], MAX_STRING_LENGTH);
+        printf("Введите строку %d: ", i + 1);
+        fgets((*stringArray)[i], MAX_STRING_LENGTH, stdin);
+
+        // Удалить символ новой строки, если он был считан
+        size_t length = strlen((*stringArray)[i]);
+        if (length > 0 && (*stringArray)[i][length - 1] == '\n') {
+            (*stringArray)[i][length - 1] = '\0';
+        }
     }
 }
 
 void PrintStrings(char** stringArray, int size) {
-    printf("String Array:\n");
+    printf("Массив строк:\n");
     for (int i = 0; i < size; ++i) {
         printf("%s\n", stringArray[i]);
     }
 }
 
-void DeleteStrings(char*** stringArray, int* size) {
+void DeleteEvenStrings(char*** stringArray, int* size) {
+    if (*size == 0) {
+        printf("Массив строк пуст.\n");
+        return;
+    }
+
     int count = 0;
 
-    // Count the number of odd strings
     for (int i = 0; i < *size; ++i) {
         if (i % 2 != 0) {
             free((*stringArray)[i]);
-        }
-        else {
+        } else {
             (*stringArray)[count++] = (*stringArray)[i];
         }
     }
 
-    // Resize the array to the new count of odd strings
     *stringArray = realloc(*stringArray, count * sizeof(char*));
 
     if (*stringArray == NULL && count != 0) {
-        fprintf(stderr, "Memory reallocation error\n");
+        fprintf(stderr, "Ошибка перераспределения памяти\n");
         exit(1);
     }
 
@@ -114,8 +140,8 @@ void DeleteStrings(char*** stringArray, int* size) {
 
 int main() {
     int numPeople;
-    printf("Enter the number of people: ");
-    scanf_s("%i", &numPeople);
+    printf("Введите количество людей: ");
+    scanf("%i", &numPeople);
 
     struct Person* peopleArray = GenArray(numPeople);
 
@@ -126,43 +152,33 @@ int main() {
     int secondSize;
     struct Person* secondArr = A_Names(peopleArray, numPeople, &secondSize);
 
-    printf("People whose names begin with A: \n");
-    for (int i = 0; i < secondSize; ++i) {
-        PrintArray(&secondArr[i]);
+    if (secondArr != NULL) {
+        printf("Люди, имена которых начинаются с A: \n");
+        for (int i = 0; i < secondSize; ++i) {
+            PrintArray(&secondArr[i]);
+        }
+
+        // Освободить память для второго массива структур Person
+        for (int i = 0; i < secondSize; ++i) {
+            free(secondArr[i].name);
+        }
+        free(secondArr);
     }
 
-    // Free memory for Person array
     for (int i = 0; i < numPeople; ++i) {
         free(peopleArray[i].name);
     }
     free(peopleArray);
 
-    // Free memory for A_Names result array
-    for (int i = 0; i < secondSize; ++i) {
-        free(secondArr[i].name);
-    }
-    free(secondArr);
-
-    // Additional steps
     int numStrings;
-    printf("Enter the number of strings: ");
-    scanf_s("%d", &numStrings);
-
+    printf("Введите количество строк: ");
+    scanf("%d", &numStrings);
     char** stringArray;
-
-    // Step 5: Form the dynamic string array and perform input
     FillStrings(&stringArray, numStrings);
-
-    // Step 6: Print the formed array
     PrintStrings(stringArray, numStrings);
-
-    // Step 7: Delete even strings from the array
-    DeleteStrings(&stringArray, &numStrings);
-
-    // Step 8: Print the resulting array
+    DeleteEvenStrings(&stringArray, &numStrings);
     PrintStrings(stringArray, numStrings);
-
-    // Step 9: Free allocated memory for string array
+    
     for (int i = 0; i < numStrings; ++i) {
         free(stringArray[i]);
     }
