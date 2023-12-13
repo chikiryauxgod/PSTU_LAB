e8dc40c2dcc967cb230886a46323f9ab0c47d3ec
Семенов Лев <130220311+chikiryauxgod@users.noreply.github.com>
2023-12-13 15:21:34 +0500

    Update main.c

--- С/CLab8/CLab8/main.c
+++ С/CLab8/CLab8/main.c
@@ -2,48 +2,56 @@
 #include <stdlib.h>
 
 struct node {
-	int value;
-	struct node* next;
+int value;
+struct node* previous;
 };
 
 void push_back(struct node** last, int data) {
-	struct node* tmp = (struct node*)malloc(sizeof(struct node));
-	tmp->value = data;
-	tmp->next = (*last);
-	(*last) = tmp;
+struct node* tmp = (struct node*)malloc(sizeof(struct node));
+tmp->value = data;
+tmp->previous = (*last);
+(*last) = tmp;
 }
 
 void for_each(struct node** last, void(*function)(int*))
 {
-	struct node* tmp = *last;
-	while (tmp) {
-		function(&tmp->value);
-		tmp = tmp->next;
-	}
+struct node* tmp = *last;
+while (tmp) {
+function(&tmp->value);
+tmp = tmp->previous;
+}
 }
 
 void erase_list(struct node** last)
 {
-	struct node* tmp = *last;
-	while (tmp) {
-		tmp = tmp->next;
-		free(*last);
-		*last = tmp;
-	}
+struct node* tmp = *last;
+while (tmp) {
+tmp = tmp->previous;
+free(*last);
+*last = tmp;
+}
+}
+void setman (int* element){
+printf("Введите значение элемента: ");
+scanf("%i", element);
+}
+
+void show(int* element){
+printf("%03d ", *element);
 }
 
 int main() {
-	struct node* head = 0;
-	push_back(&head, 11);
-	push_back(&head, 12);
-	for_each(&head, show);
-	printf("\n");
-	for_each(&head, setman)
-	push_back(&head, 13);
-	for_each(&head, show);
-	printf("\n");
-	printf("head: %LX\n", head);
-	erase_list(&head);
-	printf("head: %LX\n", head);
+struct node* head = 0;
+push_back(&head, 11);
+push_back(&head, 12);
+for_each(&head, show);
+printf("\n");
+for_each(&head, setman);
+push_back(&head, 13);
+for_each(&head, show);
+printf("\n");
+printf("head: %LX\n", head);
+erase_list(&head);
+printf("head: %LX\n", head);
 
 }
