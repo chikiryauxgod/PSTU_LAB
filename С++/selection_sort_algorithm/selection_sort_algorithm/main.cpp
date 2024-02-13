11ec5e28444f8a846bd9d1236d6d4d53d34bcb1f
Семенов Лев <lewace37@yandex.ru>
2024-02-13 20:00:52 +0500

    upd

--- С++/selection_sort_algorithm/selection_sort_algorithm/main.cpp
+++ С++/selection_sort_algorithm/selection_sort_algorithm/main.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+template <typename T>
+int find_smallest(const vector<T>& arr)
+{
+	T smallest = arr[0];
+	int smallest_ind = 0;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (arr[i] < smallest)
+		{
+			smallest = arr[i];
+			smallest_ind = i;
+		}
+	}
+	return smallest_ind;
+}
+
+template <typename T>
+vector <T> selection_sort(vector <T> arr) {
+	vector <T> sorted;
+	{
+		while (!arr.empty())
+		{
+			int smallest_ind = find_smallest(arr);
+			sorted.push_back(arr[smallest_ind]);
+			arr.erase(arr.begin() + smallest_ind);
+
+		}
+
+	}
+	return sorted;
+}
 
 
 
-
-
-
+ 
 
 int main()
 {
+	std::vector<double> arr = { 1, 1.2, 3, 0, -1.0, 0.5, 100, -99 };
+	vector<double> sorted = selection_sort(arr);
 
+	cout << "Sorted array: ";
+	for (double num : sorted)
+	{
+		cout << num << " ";
+	}
+	cout << endl;
 }
