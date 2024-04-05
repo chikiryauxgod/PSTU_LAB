e12160f7efcd9345f34fd5f517e269009313212f
Семенов Лев <lewace37@yandex.ru>
2024-04-06 00:37:09 +0500

    upd

--- С++/single-linked-list/main.cpp
+++ С++/single-linked-list/main.cpp
@@ -11,6 +11,11 @@ public:
     void push_back(T data);
     int GetSize() { return Size; }
     T& operator[](const int index);
+    void pop_front();
+    void clear();
+    void insert(T value, int index);
+    void push_front(T data);
+    void removeAt(int index);
 
 private:
     template<typename T>
@@ -40,7 +45,7 @@ List<T>::List()
 template<typename T>
 List<T>::~List()
 {
-    
+    clear();
 }
 
 template<typename T>
@@ -79,22 +84,94 @@ T& List<T>::operator[](const int index)
     }
 }
 
+template<typename T>
+void List<T>::pop_front()
+{
+    Node<T> *temp = head;
+    head = head->pNext;
+    delete temp;
+    Size--;
+}
+
+template<typename T>
+void List<T>::clear()
+{
+    while (Size)
+    {
+        pop_front();
+        cout << "The element was deleted." << endl;
+    }
+}
+
+template<typename T>
+void List<T>::insert(T data, int index)
+{
+    if (index == 0)
+    {
+        push_front(data);
+    }
+    else
+    {
+        Node<T>* previous = this->head;
+        for (int i = 0; i < index - 1; i++)
+        {
+            previous = previous->pNext;
+        }
+
+        Node<T>* newNode = new Node<T>(data, previous->pNext);
+        previous->pNext = newNode;
+        Size++;
+    }
+}  
+
+    
+
+template<typename T>
+void List<T>::push_front(T data)
+{
+    head = new Node <T>(data, head);
+    Size++;
+}
+
+template<typename T>
+void List<T>::removeAt(int index)
+{
+    if (index == 0)
+    {
+        pop_front(data);
+    }
+    else
+    {
+        Node<T>* previous = this->head;
+        for (int i = 0; i < index - 1; i++)
+        {
+            previous = previous->pNext;
+        }
+
+        Node<T>* toDelete = toDelete->pNext;
+        previous->pNext = toDelete->pNext;
+        delete toDelete;
+        Size--;
+    }
+
+    
+}
+
 int main()
 {
     setlocale(LC_ALL, "ru");
 
     List<int> lst;
-    int numbersCount;
-    cin >> numbersCount;
-    for (int i = 0; i < numbersCount; i++)
-    {
-        lst.push_back(rand() % 50);
-    }
-
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(3);
+    lst.pop_front();
 
     for (int i = 0; i < lst.GetSize(); i++)
     {
         cout << lst[i] << endl;
     }
+
+    lst.clear();
     return 0;
 }
