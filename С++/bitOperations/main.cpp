6360b0e604ff2f6875b5565ad605e211b8081fba
Семенов Лев <lewace37@yandex.ru>
2024-09-11 22:01:42 +0500

    new

---
+++ С++/bitOperations/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = -1;
+	int b = a >> 1;
+	int c = a << 1;
+	cout << b << " =  >> 2" << endl;
+	cout << c << " =  << 2" << endl;
+
+
+
+
+	return 0;
+}
